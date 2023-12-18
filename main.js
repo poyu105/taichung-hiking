2e418a8096c44e94fcd13ea5c21dce627b577e2b
benson <benson08281015@gmail.com>
2023-12-18 11:21:35 +0800

    connect json

--- main.js
+++ main.js
@@ -1,99 +1,98 @@
-document.addEventListener("DOMContentLoaded",function(){
-    
-})
+// window.onload = function () {
+//   // addCard();
+//   getData();
+//   // setInterval(getData, 30000);
+// };
+
 //card優化(動態添加) ***勿刪***
-function addCard(){
-    //計算post-content子元素(card)個數
-    var fatherEle=document.getElementById("post-content");//取得div, id=post-content 父元素
-    var contentEle=fatherEle.getElementsByClassName("card");//取得fatherEle下的div, class=card子元素
-    console.log("目前card數量: "+contentEle.length);//顯示contentEle長度(個數) (測試用) 
+// function addCard() {
+//   //計算post-content子元素(card)個數
+//   var fatherEle = document.getElementById("post-content"); //取得div, id=post-content 父元素
+//   var contentEle = fatherEle.getElementsByClassName("card"); //取得fatherEle下的div, class=card子元素
+//   console.log("目前card數量: " + contentEle.length); //顯示contentEle長度(個數) (測試用)
 
-    //計算父/子元素寬度
-    var container=document.getElementById("post-content");
-    var containerWidth=parseFloat(container.offsetWidth);//container 的寬度
+//   //計算父/子元素寬度
+//   var container = document.getElementById("post-content");
+//   var containerWidth = parseFloat(container.offsetWidth); //container 的寬度
 
-    var item=document.querySelector(".card");//獲取class="card"元素 
-    var itemWidth=parseFloat(window.getComputedStyle(item,null).getPropertyValue("width"))+20;//item的寬度,20為card固定margin L/R
+//   var item = document.querySelector(".card"); //獲取class="card"元素
+//   var itemWidth =
+//     parseFloat(window.getComputedStyle(item, null).getPropertyValue("width")) +
+//     20; //item的寬度,20為card固定margin L/R
 
-    console.log("containerWidth: "+containerWidth);
-    console.log("itemWidth: "+itemWidth);
+//   console.log("containerWidth: " + containerWidth);
+//   console.log("itemWidth: " + itemWidth);
 
-    //計算每一列(per-row)包含多少元素(card), count最終值-1
-    var count=1;
-    while(count<10){
-        console.log("目前count為: "+count);
-        console.log("itemWidth*count為: "+itemWidth*count)
-        console.log("containerWidth為: "+containerWidth);
-        
-        if((itemWidth*count)>containerWidth){
+//   //計算每一列(per-row)包含多少元素(card), count最終值-1
+//   var count = 1;
+//   while (count < 10) {
+//     console.log("目前count為: " + count);
+//     console.log("itemWidth*count為: " + itemWidth * count);
+//     console.log("containerWidth為: " + containerWidth);
 
-            break;
-        }
-        else{
-            count++;
-        }
-        //避免無窮迴圈, 預設只跑10次
-        if(count>10){
-            break;
-        }
-    }
+//     if (itemWidth * count > containerWidth) {
+//       break;
+//     } else {
+//       count++;
+//     }
+//     //避免無窮迴圈, 預設只跑10次
+//     if (count > 10) {
+//       break;
+//     }
+//   }
 
-    //判斷是否元素個數是否為滿版
-    const sumOfCards=contentEle.length//card總數
-    var missEle=false;
-    if(sumOfCards%(count-1)!=0){
-        missEle=true;
-    }
-    //增加card元素(非滿版時執行)
-    if(missEle){
-        count--;//count最終值必須-1
-        var missCount=0;//缺少元素(card)個數
-        var tempSum=sumOfCards;//cp sumOfCards
-        //計算缺少元素(card)個數
-        for(i=1;(tempSum%count)!=0;i++){
-            tempSum++;
-            missCount=i;
-        }
-        console.log("missCount: "+missCount);
-        //新增element
-        for(i=0;i<missCount;i++){
-            var cardDiv=document.createElement("div");
-            cardDiv.setAttribute('class','card');
-            cardDiv.innerHTML=
-            `<img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-            </ul>
-            <div class="card-body">
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-            </div>`;
-            document.getElementById("post-content").appendChild(cardDiv);
-        }
-    }
-}
+//   //判斷是否元素個數是否為滿版
+//   const sumOfCards = contentEle.length; //card總數
+//   var missEle = false;
+//   if (sumOfCards % (count - 1) != 0) {
+//     missEle = true;
+//   }
+//   //增加card元素(非滿版時執行)
+//   if (missEle) {
+//     count--; //count最終值必須-1
+//     var missCount = 0; //缺少元素(card)個數
+//     var tempSum = sumOfCards; //cp sumOfCards
+//     //計算缺少元素(card)個數
+//     for (i = 1; tempSum % count != 0; i++) {
+//       tempSum++;
+//       missCount = i;
+//     }
+//     console.log("missCount: " + missCount);
+//     //新增element
+//     for (i = 0; i < missCount; i++) {
+//       var cardDiv = document.createElement("div");
+//       cardDiv.setAttribute("class", "card");
+//       cardDiv.innerHTML = `<img src="..." class="card-img-top" alt="...">
+//             <div class="card-body">
+//             <h5 class="card-title">Card title</h5>
+//             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//             </div>
+//             <ul class="list-group list-group-flush">
+//             <li class="list-group-item">An item</li>
+//             <li class="list-group-item">A second item</li>
+//             <li class="list-group-item">A third item</li>
+//             </ul>
+//             <div class="card-body">
+//             <a href="#" class="card-link">Card link</a>
+//             <a href="#" class="card-link">Another link</a>
+//             </div>`;
+//       document.getElementById("post-content").appendChild(cardDiv);
+//     }
+//   }
+// }
 
 //頁面載入時執行
-window.onload=function(){
-    addCard();
-}
-//時時監聽視窗比例
-window.addEventListener('load',function(){
-    let lastPixelRatio=window.innerWidth;
-    window.addEventListener('resize',function(){
-        let currentPixelRatio=window.innerWidth;
-        if(currentPixelRatio!=lastPixelRatio){
-            addCard();
-            lastPixelRatio=currentPixelRatio;
-        }
-    })
-})
+// //時時監聽視窗比例
+// window.addEventListener("load", function () {
+//   let lastPixelRatio = window.innerWidth;
+//   window.addEventListener("resize", function () {
+//     let currentPixelRatio = window.innerWidth;
+//     if (currentPixelRatio != lastPixelRatio) {
+//       addCard();
+//       lastPixelRatio = currentPixelRatio;
+//     }
+//   });
+// });
 
 //card優化2(動態添加，連接json) ***勿刪***  https://israynotarray.com/javascript/20190505/1432256317/
 
@@ -176,5 +175,78 @@ function displayData(data) {
 
 }
 */
+//取得資料
+function getData() {
+  $.ajax({
+    url: "./臺中市健行步道資料1120915(修增加經緯度).json",
+    method: "GET",
+    data: {
 
-  
+    },
+    dataType: "json",
+    async: true,
+    success: function (data) {
+      let trails = data.trails;
+      let dataLengths = trails.length;
+      //初始化卡片
+      $("#post-content").empty();
+      for (let i = 0; i < dataLengths; i++) {
+        console.log(i);
+        let cardDiv1 = $("<div>").addClass("card");
+        let cardImg = $("<img>")
+          .addClass("card-img-top")
+          .attr("src", trails[i]["picture"]);
+        let cardDiv2 = $("<div>").addClass("card-body");
+        let cardTitle = $("<h5>")
+          .addClass("card-title")
+          .html(trails[i]["titleName"]);
+        let cardUl = $("<ul>").addClass("list-group list-group-flush");
+        let cardLi1 = $("<li>")
+          .addClass("list-group-item")
+          .html("地區：" + trails[i]["area"]);
+        let cardLi2 = $("<li>")
+          .addClass("list-group-item")
+          .html(
+            "經度：" + trails[i]["longitude"]+"<br>" +"緯度：" + trails[i]["latitude"]
+          );
+        let cardLi3 = $("<li>")
+          .addClass("list-group-item")
+          .html("分類：" + trails[i]["classification"]);
+        let cardLi4 = $("<li>")
+          .addClass("list-group-item")
+          .html("等級：" + trails[i]["level"]);
+        let cardLi5 = $("<li>")
+          .addClass("list-group-item")
+          .html("設施：" + trails[i]["facilities"]);
+        let cardLi6 = $("<li>")
+          .addClass("list-group-item")
+          .html("維護單位：" + trails[i]["maintenanceUnit"]);
+        let cardLi7 = $("<li>")
+          .addClass("list-group-item")
+          .html("長度：" + trails[i]["length"]);
+        let cardLi8 = $("<li>")
+          .addClass("list-group-item")
+          .html("評分：" + trails[i]["evaluate"]);
+        cardUl.append(
+          cardLi1,
+          cardLi2,
+          cardLi3,
+          cardLi4,
+          cardLi5,
+          cardLi6,
+          cardLi7,
+          cardLi8
+        );
+        cardDiv2.append(cardTitle);
+        cardDiv1.append(cardImg, cardDiv2, cardUl);
+        $("#post-content").append(cardDiv1);
+      }
+    },
+    error: function () {
+      alert("error");
+    },
+  });
+}
+$(document).ready(function () {
+  getData();
+});
